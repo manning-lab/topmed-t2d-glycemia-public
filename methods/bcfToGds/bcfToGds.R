b3b938557562b00ca19c70aef5e1b898e89cfbfb
Tim Majarian <30277822+tmajaria@users.noreply.github.com>
2017-09-01 10:45:24 -0400

    Update bcfToGds.R

--- methods/bcfToGds/bcfToGds.R
+++ methods/bcfToGds/bcfToGds.R
@@ -3,7 +3,6 @@
 library(SeqArray)
 input_args <- commandArgs(trailingOnly=T)
 vcf <- input_args[1] 
-#method <- input_args[2]
 method <- "biallelic.only"
 
 spl <- unlist(strsplit(vcf,"/",perl=TRUE))
@@ -12,4 +11,9 @@ spl <- unlist(strsplit(spl[length(spl)], "\\.",perl=TRUE))
 gds <- unlist(paste(paste(spl[1:length(spl)-1], collapse = '.'),".gds",sep=""))
 gds <- gds[length(gds)]
 
+
 outfile <- seqVCF2GDS(vcf, gds, storage.option="LZMA_RA", verbose=FALSE)
+
+fileConn<-file("output.txt")
+writeLines(gds, fileConn)
+close(fileConn)
