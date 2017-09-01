dba46eda3188857e63b72c417c261933f41f7a80
Tim Majarian <30277822+tmajaria@users.noreply.github.com>
2017-09-01 09:55:33 -0400

    Update hello.R

--- methods/bcfToGds/hello.R
+++ methods/bcfToGds/hello.R
@@ -1,3 +1,7 @@
-fileConn<-file("output.txt")
-writeLines(c("Hello","World"), fileConn)
-close(fileConn)
+input_args <- commandArgs(trailingOnly=T)
+fname <- input_args[1] 
+d = read.table(fname)
+# fileConn<-file("output.txt")
+# writeLines(c("Hello","World"), fileConn)
+# close(fileConn)
+write(d, file = "output.txt")
