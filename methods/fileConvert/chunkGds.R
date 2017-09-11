6a7b465c2f94e4320070086ddd334b7eb0615eb1
Timothy Majarian <tmajaria@wm059-b32.broadinstitute.org>
2017-09-11 10:56:41 -0400

    adjusted filepath

--- methods/fileConvert/chunkGds.R
+++ methods/fileConvert/chunkGds.R
@@ -5,7 +5,7 @@ input_args <- commandArgs(trailingOnly=T)
 gds.file <- input_args[1]
 outbase <- basename(gds.file)
 outbase <- substr(outbase, 0, nchar(outbase)-4)
-outbase <- paste(getwd(),"/",outbase,sep="")
+#outbase <- paste(getwd(),"/",outbase,sep="")
 
 # https://stackoverflow.com/questions/3318333/split-a-vector-into-chunks-in-r
 chunk <- function(x,n) {
