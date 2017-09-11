08aaba896361c258062c6cdad85dddf41fed0fcb
Timothy Majarian <tmajaria@wm059-b32.broadinstitute.org>
2017-09-11 10:04:53 -0400

    added chunking function for gds files

---
+++ methods/fileConvert/chunkGds.R
@@ -0,0 +1,27 @@
+# break full genome gds files into chromosome->chromosome chunk files
+
+library(SeqArray)
+input_args <- commandArgs(trailingOnly=T)
+gds.file <- inputs_args[1]
+outbase <- basename(gds.file)
+outbase <- substr(outbase, 0, nchar(outbase)-4)
+
+# https://stackoverflow.com/questions/3318333/split-a-vector-into-chunks-in-r
+chunk <- function(x,n) {
+  f <- sort(rep(1:(trunc(length(x)/n)+1),n))[1:length(x)]
+  return(split(x,f))
+}
+
+gds <- seqOpen(gds.file)
+variant.id <- seqGetData(gds, "variant.id")
+variant.id.chunks <- chunk(variant.id,100)
+
+# loop through the chunks
+for(j in 1:length(variant.id.chunks)) {
+  seqSetFilter(gds, variant.id=variant.id.chunks[[j]])
+  
+  seqExport(gds, paste(outbase,".chunk",j,".gds",sep=""))
+  # reset filter
+  seqResetFilter(gds)
+}
+seqClose(gds)
