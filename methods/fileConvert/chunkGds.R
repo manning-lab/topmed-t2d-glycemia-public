79d96ea63c319a279a5bc5605e757aa743c7d478
Timothy Majarian <tmajaria@wm059-b32.broadinstitute.org>
2017-09-13 11:42:14 -0400

    changed for testing

--- methods/fileConvert/chunkGds.R
+++ methods/fileConvert/chunkGds.R
@@ -15,7 +15,7 @@ chunk <- function(x,n) {
 
 gds <- seqOpen(gds.file)
 variant.id <- seqGetData(gds, "variant.id")
-variant.id.chunks <- chunk(variant.id,400000)
+variant.id.chunks <- chunk(variant.id,1000)
 
 # loop through the chunks
 for(j in 1:length(variant.id.chunks)) {
