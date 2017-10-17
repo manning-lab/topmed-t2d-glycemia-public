d02ef7cd7fbaa490a65a8faf64288c2308d4d25e
Timothy Majarian <tmajaria@wm059-b32.fios-router.home>
2017-10-17 05:56:53 -0400

    work

--- workflows/aggregateAssociation/groupByGene.R
+++ workflows/aggregateAssociation/groupByGene.R
@@ -12,9 +12,16 @@ anno.value <- c("splice_acceptor_variant","splice_donor_variant","splice_region_
 states.names <- c("active_enhancer_1","active_enhancer_2","active_tss")
 minmaf <- 0.01
 library(SeqVarTools)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(GenomicRanges)
+library(rtracklayer)
+library(data.table)
+library(SeqArray)
 
-source("https://bioconductor.org/biocLite.R")
-biocLite("SeqVarTools")
+# source("https://bioconductor.org/biocLite.R")
+# biocLite("SeqVarTools")
 # genes.all.file <- "/Users/tmajaria/Documents/projects/topmed/data/varshney/ensembl_genes.csv"
 # genes.pan.file <- "/Users/tmajaria/Documents/projects/topmed/data/varshney/gtex/gtex.v6p.pancreas.expression.min.rpkm.0.1.txt"
 # gds.file <- "/Users/tmajaria/Documents/projects/topmed/data/varshney/freeze4.chr10.pass.gtonly.minDP10.genotypes.gds"
@@ -54,31 +61,45 @@ biocLite("SeqVarTools")
   }
   
   ## open gds file
-  library(SeqArray)
+  
   gds.data <- seqOpen(gds.file)
   chr_num <- seqGetData(gds.data,"chromosome")[1]
   chr <- paste("chr",chr_num,sep="")
   
   ## load genes first
-  library(data.table)
+  
   genes.all <- fread(genes.all.file,sep=",",header=T,stringsAsFactors=FALSE,showProgress=TRUE,data.table=FALSE)
   genes.all <- genes.all[order(genes.all$hgnc_symbol),]
   genes.pan.raw <- fread(genes.pan.file,sep="\t",header=T,stringsAsFactors=FALSE,showProgress=TRUE,data.table=FALSE)
   genes.pan <- genes.all[genes.all$ensembl_gene_id %in%  genes.pan.raw[genes.pan.raw$RPKM>=2 & genes.pan.raw$Gene_Type=="protein_coding",1],]
   genes.pan <- genes.pan[!duplicated(genes.pan$ensembl_gene_id),]
+
+  rm(genes.all)
+  rm(genes.pan.raw)
   
   ## remap to hg19
-  library(rtracklayer)
+  
   hg38tohg19 <- import.chain(chain.file) # (http://hgdownload.cse.ucsc.edu/goldenPath/hg38/liftOver/)
   genes.hg38 <- GRanges(seqnames=rep(chr,length(genes.pan$chromosome_name)),ranges=IRanges(start=genes.pan$start_position, end=genes.pan$end_position),symbol=genes.pan$hgnc_symbol,id=genes.pan$ensembl_gene_id)
   genes.hg19 <- unlist(liftOver(genes.hg38, hg38tohg19))
   
-  library(GenomicRanges)
+  rm(hg38tohg19)
+  rm(genes.hg38)
+  
   genes.pan.gr <- GRanges(seqnames=rep(chr_num,length(genes.hg19$id)),ranges=IRanges(start=start(genes.hg19)-genes.pad, end=end(genes.hg19)+genes.pad),symbol=genes.hg19$symbol,id=genes.hg19$id)
   genes.pan.gr_nopad <- GRanges(seqnames=rep(chr_num,length(genes.hg19$id)),ranges=IRanges(start=start(genes.hg19), end=end(genes.hg19)),symbol=genes.hg19$symbol,id=genes.hg19$id)
+
+  rm(genes.hg19)
   
   ## load annotations
   anno.raw <- fread(anno.file,sep="\t",header=T,stringsAsFactors=FALSE,showProgress=TRUE,data.table=FALSE)
+  # anno.raw <- anno.raw[,c(1:3,5,7)]
+  print(head(anno.raw$VEP_ensembl_Gene_ID))
+  # anno.raw <- anno.raw[order(anno.raw$VEP_ensembl_Gene_ID),]
+  a <- anno.raw$VEP_ensembl_Gene_ID != "."
+  anno.sub_one <- anno.raw[a,]
+
+  rm(anno.raw)
   
   ## load chromatin states
   states.raw <- fread(states.file,sep=",",header=T,stringsAsFactors=FALSE,showProgress=TRUE,data.table=FALSE)
@@ -86,6 +107,9 @@ biocLite("SeqVarTools")
   states.subset[,4] <- sub("chr","",states.subset[,4]) # 1329
   states.gr <- GRanges(seqnames=states.subset$chr,ranges=IRanges(start=states.subset$start,end=states.subset$stop),state=states.subset$type)
   
+  rm(states.raw)
+  rm(states.subset)
+
   ## collecting the groups
   groups <- list()
   
@@ -102,13 +126,10 @@ biocLite("SeqVarTools")
     seqSetFilter(gds.data,cur_gene.gr)
     
     ## get the variants to a better format
-    library(SeqVarTools)
-    library(dplyr)
-    library(tidyr)
     gds.df <- .expandAlleles(gds.data)
     # library(SeqVarTools)
     # gds.df$maf <- alleleFrequency(gds.data,n=1)
-    ref.freq <- seqAlleleFreq(gds.data, .progress=TRUE)
+    ref.freq <- seqAlleleFreq(gds.data)
     gds.df$maf <- pmin(ref.freq, 1-ref.freq)
 
     ## subset the variants to those with maf<1% and maf > 0
@@ -118,10 +139,12 @@ biocLite("SeqVarTools")
     names(gds.gr) <- gds.df.umaf$variant.id
     
     ## subset annotations to our range
-    anno.subset <- anno.raw[anno.raw$VEP_ensembl_Gene_ID == cur_gene.gr$id,]
     
+    print(head(cur_gene.gr))
+    anno.subset <- anno.sub_one[anno.sub_one$VEP_ensembl_Gene_ID == cur_gene.gr$id,]
+    print("here")
     ## get only those annotations that have our consequences
-    library(stringr)
+    
     anno.subvals <- anno.subset[apply(as.matrix(anno.subset$VEP_ensembl_Consequence), 1, function(x) any(sapply(anno.value, function(y) str_detect(unlist(str_split(x, pattern=",")),y)))),]
     anno.df <- data.frame(pos=anno.subvals$pos,alt=as.vector(anno.subvals$alt),annotation=as.vector(anno.subvals$VEP_ensembl_Consequence))
     gds_match <- data.frame(pos=gds.df.umaf$position,alt=as.vector(gds.df.umaf$allele))
@@ -143,10 +166,10 @@ biocLite("SeqVarTools")
     anno.snps$annotation <- anno_paste3[anno_paste3[,1] %in% gds_paste,2]
     
     ## get variants within the chromatin states
-    states.subset <- states.gr[end(states.gr)>start(cur_gene.gr),]
-    states.subset <- states.subset[start(states.subset)<end(cur_gene.gr),]
+    states.subset_o <- states.gr[end(states.gr)>start(cur_gene.gr),]
+    states.subset <- states.subset_o[start(states.subset_o)<end(cur_gene.gr),]
     states.hits <- findOverlaps(gds.gr,states.subset)
-    states.snps <- gds.gr[queryHits(states.hits),]
+    states.snps_o <- gds.gr[queryHits(states.hits),]
     states.snps <- gds.df.umaf[queryHits(states.hits),]
     states.snps$annotation <- states.subset$state[subjectHits(states.hits)]
     
