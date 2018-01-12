771dac698c15ebfaa24a98a3367d4f32a8d720c2
Timothy Majarian <tmajaria@wm059-b32.broadinstitute.org>
2018-01-12 12:59:52 -0500

    commented and now makes figures for continuous traits

--- methods/phenotypeSummary/phenotypeSummary.R
+++ methods/phenotypeSummary/phenotypeSummary.R
@@ -5,109 +5,182 @@ covars <- unlist(strsplit(input_args[3],","))
 label <- input_args[4]
 cohort_column <- input_args[5]
 
+#####
 # ped.file <- "/Users/tmajaria/Documents/projects/topmed/data/test_inputs/phenotypes/Pooled_AFEU_WesselJ_25AUG2017_T2D.csv"
-# outcome <- "t2d_ctrl"
+# outcome <- "t2d_ctrl" # outcome <- "last_exam_bmi"
 # covars <- unlist(strsplit("last_exam_age,sex,study",","))
 # label <- "testing"
 # cohort_column <- "study"
+#####
 
-# Load ped.data data
+# Load phenotype data
 library(data.table)
 ped.data <- fread(ped.file,header=T,stringsAsFactors=FALSE,showProgress=TRUE,data.table=FALSE)
-ped.data = ped.data[,c(outcome,covars)]
+ped.data = na.omit(as.data.frame(ped.data[,unique(c(cohort_column,outcome,covars)),drop=F]))
 
-# determine outcome type
+# Determine outcome type
 is.continuous <- ifelse(length(unique(ped.data[,outcome])) < 4 ,F,T)
 
-# get number of outcomes
+# Get number of possible outcomes
 if (!(is.continuous)){
   outcome.vals <- sort(unique(ped.data[,outcome]))
+} else {
+  covars = c(outcome,covars)
 }
 
-# remove cohort column and any study based columns if its in covars
+# Remove cohort column and any study based columns if its in covars
 covars = covars[!(covars %in% c(cohort_column,"STUDY_ANCESTRY","study_ancestry","topmed_project"))]
 
-
-
-# determine types of covars
+# Determine types of covars
 val.type <- apply(ped.data[,covars], 2, function(x) ifelse(length(unique(x)) <= 2 ,"categorical","continuous"))
 
+# Gather each cohorts stats in a list to be combined later
 stats <- list()
-# row_names <- c()
-for (study in unique(ped.data[,cohort_column])){
-  row_names <- c(paste(study,"total",sep=" "))
-  for (v in outcome.vals){
-    row_names <- c(row_names,paste(study,":",outcome,"=",v,sep=" "))
-  }
-  # row_names <- c(row_names,paste(study,"total",sep=" "),paste(study,"controls",sep=" "),paste(study,"cases",sep=" "))
-  ped.cur = ped.data[ped.data[,cohort_column] == study,]
-  total <- length(ped.cur[,1])
-  samp <- c(total)
-  for (v in outcome.vals){
-    samp <- c(samp, length(ped.cur[ped.cur[,outcome] == v,1]))
-  }
-  
-  
-  all_dat <- data.frame(V1 = row_names, Samples=samp)
-  # total_dat <- c(length(ped.cur[,1]))
-  # case_dat <- c(length(ped.cur[ped.cur[,outcome] == 1,1]))
-  # ctrl_dat <- c(length(ped.cur[ped.cur[,outcome] == 0,1]))
-  for (c in covars[val.type == "continuous"]){
-  #   if (c == cohort_column){
-  #     next
-  #   }
-    # total_dat <- c(total_dat, mean(ped.cur[,c], na.rm = T), median(ped.cur[,c], na.rm = T), sd(ped.cur[,c], na.rm = T), min(ped.cur[,c], na.rm = T), max(ped.cur[,c], na.rm = T))
-    # case_dat <- c(case_dat, mean(ped.cur[ped.cur[,outcome] == 1,c], na.rm = T), median(ped.cur[ped.cur[,outcome] == 1,c], na.rm = T), sd(ped.cur[ped.cur[,outcome] == 1,c], na.rm = T), min(ped.cur[ped.cur[,outcome] == 1,c], na.rm = T), max(ped.cur[ped.cur[,outcome] == 1,c], na.rm = T))
-    # ctrl_dat <- c(ctrl_dat, mean(ped.cur[ped.cur[,outcome] == 0,c], na.rm = T), median(ped.cur[ped.cur[,outcome] == 0,c], na.rm = T), sd(ped.cur[ped.cur[,outcome] == 0,c], na.rm = T), min(ped.cur[ped.cur[,outcome] == 0,c], na.rm = T), max(ped.cur[ped.cur[,outcome] == 0,c], na.rm = T))
-    means <- c(mean(ped.cur[,c]))
-    medians <- c(median(ped.cur[,c]))
-    sds <- c(sd(ped.cur[,c]))
-    mins <- c(min(ped.cur[,c]))
-    maxes <- c(max(ped.cur[,c]))
-    for (v in outcome.vals){
-      ped.new <- ped.cur[ped.cur[,outcome] == v,c]
-      means = c(means,mean(ped.new))
-      medians = c(medians, median(ped.new))
-      sds = c(sds, sd(ped.new))
-      mins = c(mins, min(ped.new))
-    }
-    
-    all_dat <- cbind(all_dat,means,medians,sds,mins,maxes)
-    
-  }
-  
-  
-  ############## need to do below this point
-  for (c in covars[val.type == "categorical"]){
-    if (c == outcome){
-      next
-    }
-    
-    
-    curvals <- unique(ped.cur[,c])
-    for (cv in curvals){
-      
-      pd.c <- ped.cur[ped.cur[,c] == cv,]
-      percents <- c(length(pd.c[,1])/total)
-      for (v in outcome.vals){
-        ped.new <- pd.c[pd.c[,outcome] == v,c]
-        percents <- c(percents,length(ped.new)/length(ped.cur[ped.cur[,outcome] == v,1]))
-      }
-      all_dat <- cbind(all_dat,percents)  
-    }
-    
-    
-    
-    # uvals <- unique(ped.cur[,c])
-    # total_dat <- c(total_dat, sum(ped.cur[,c] == uvals[1])/length(ped.cur[,1]),sum(ped.cur[,c] == uvals[2])/length(ped.cur[,1]))
-    # case_dat <- c(case_dat, sum(ped.cur[ped.cur[,outcome] == 1,c] == uvals[1])/length(ped.cur[ped.cur[,outcome] == 1,c]),sum(ped.cur[ped.cur[,outcome] == 1,c] == uvals[2])/length(ped.cur[ped.cur[,outcome] == 1,c]))
-    # ctrl_dat <- c(ctrl_dat, sum(ped.cur[ped.cur[,outcome] == 0,c] == uvals[1])/length(ped.cur[ped.cur[,outcome] == 0,c]),sum(ped.cur[ped.cur[,outcome] == 0,c] == uvals[2])/length(ped.cur[ped.cur[,outcome] == 0,c]))
-  }
-  
-  # all_dat <- cbind(all_dat,percents)
-  stats[[length(stats)+1]] <- all_dat
-}
 
+# Two different loops for catgorical vs continuous
+if (!(is.continuous)){
+  # Loop through cohorts
+  for (study in unique(ped.data[,cohort_column])){
+    
+    # Generate the row names that we'll use
+    row_names <- c(paste(study,"total",sep=" "))
+    for (v in outcome.vals){
+      row_names <- c(row_names,paste(study,":",outcome,"=",v,sep=" "))
+    }
+    
+    # Subset the phenotype data down to only the study we're currently using
+    ped.cur = ped.data[ped.data[,cohort_column] == study,]
+    
+    # Get the total number of samples in this cohort
+    total <- length(ped.cur[,1])
+    
+    # Start collecting numbers of samples per outcome condition
+    samp <- c(total)
+    for (v in outcome.vals){
+      samp <- c(samp, length(ped.cur[ped.cur[,outcome] == v,1]))
+    }
+    
+    # This is where we will store all stats for this cohort
+    all_dat <- data.frame(V1 = row_names, Samples=samp)
+    
+    # Loop through the continuous covariates to calculate mean, median, std, min, max
+    for (c in covars[val.type == "continuous"]){
+      if (c == cohort_column){
+        next
+      }
+      
+      # Store the values for the whole cohort first
+      means <- c(mean(ped.cur[,c]))
+      medians <- c(median(ped.cur[,c]))
+      sds <- c(sd(ped.cur[,c]))
+      mins <- c(min(ped.cur[,c]))
+      maxes <- c(max(ped.cur[,c]))
+      
+      # Loop through each outcome condition and calculate values
+      for (v in outcome.vals){
+        ped.new <- ped.cur[ped.cur[,outcome] == v,c]
+        means = c(means,mean(ped.new))
+        medians = c(medians, median(ped.new))
+        sds = c(sds, sd(ped.new))
+        mins = c(mins, min(ped.new))
+      }
+      
+      # Store the values back in the whole data frame
+      all_dat <- cbind(all_dat,means,medians,sds,mins,maxes)
+      
+    }
+    
+    # Loop through the categorical covariates
+    for (c in covars[val.type == "categorical"]){
+      if (c == cohort_column){
+        next
+      }
+      
+      # Get all possible values of the current covar
+      curvals <- unique(ped.cur[,c])
+      
+      # Loop through the possible values of this covar
+      for (cv in curvals){
+        
+        # Subset the phenotypes to only cases with this covar value
+        pd.c <- ped.cur[ped.cur[,c] == cv,]
+        
+        # Total percent with this value for the covar
+        percents <- c(length(pd.c[,1])/total)
+        
+        # Calculate percents of samples with this value over total with given condition
+        for (v in outcome.vals){
+          ped.new <- pd.c[pd.c[,outcome] == v,c]
+          percents <- c(percents,length(ped.new)/length(ped.cur[ped.cur[,outcome] == v,1]))
+        }
+        
+        # Store back in the total data frame
+        all_dat <- cbind(all_dat,percents)  
+      }
+    }
+    
+    # Store the cohort specific stats in the list
+    stats[[length(stats)+1]] <- all_dat
+  }
+} else {
+  for (study in unique(ped.data[,cohort_column])){
+    
+    # Subset the phenotype data down to only the study we're currently using
+    ped.cur = ped.data[ped.data[,cohort_column] == study,]
+    
+    # Get the total number of samples in this cohort
+    total <- length(ped.cur[,1])
+    
+    # This is where we will store all stats for this cohort
+    all_dat <- data.frame(V1 = study, Samples=total)
+    
+    # Loop through the continuous covariates to calculate mean, median, std, min, max
+    for (c in covars[val.type == "continuous"]){
+      if (c == cohort_column){
+        next
+      }
+      
+      # Store the values for the whole cohort first
+      means <- c(mean(ped.cur[,c]))
+      medians <- c(median(ped.cur[,c]))
+      sds <- c(sd(ped.cur[,c]))
+      mins <- c(min(ped.cur[,c]))
+      maxes <- c(max(ped.cur[,c]))
+      
+      # Store the values back in the whole data frame
+      all_dat <- cbind(all_dat,means,medians,sds,mins,maxes)
+      
+    }
+    
+    # Loop through the categorical covariates
+    for (c in covars[val.type == "categorical"]){
+      if (c == cohort_column){
+        next
+      }
+      
+      # Get all possible values of the current covar
+      curvals <- unique(ped.cur[,c])
+      
+      # Loop through the possible values of this covar
+      for (cv in curvals){
+        
+        # Subset the phenotypes to only cases with this covar value
+        pd.c <- ped.cur[ped.cur[,c] == cv,]
+        
+        # Total percent with this value for the covar
+        percents <- c(length(pd.c[,1])/total)
+        
+        # Store back in the total data frame
+        all_dat <- cbind(all_dat,percents)  
+      }
+    }
+    
+    # Store the cohort specific stats in the list
+    stats[[length(stats)+1]] <- all_dat
+  }
+}
+# Determine the column and row names for the data frame
+# Continuous covars go first
 cont_vals <- covars[val.type == "continuous"]
 cont_cols <- c()
 for (c in cont_vals){
@@ -116,6 +189,8 @@ for (c in cont_vals){
   }
   cont_cols <- c(cont_cols, paste(c,"mean",sep=" "), paste(c,"median",sep=" "), paste(c,"STD",sep=" "), paste(c,"min",sep=" "), paste(c,"max",sep=" "))
 }
+
+# Next are categorical covars
 dich_vals <- covars[val.type == "categorical"]
 dich_cols <- c()
 for (c in dich_vals){
@@ -126,109 +201,59 @@ for (c in dich_vals){
   dich_cols <- c(dich_cols, paste(c,"%",uvals[1],sep=" "),paste(c,"%",uvals[2],sep=" "))
 }
 
+# Add samples to headers and combine
 col_names <- c("Samples",cont_cols,dich_cols)
+
+# Combine list to data frame containing all cohorts
 stats.df <- as.data.frame(do.call(rbind,stats))#, row.names = row_names)
+
+# Move the row names to row.names of data frame
 row.names(stats.df) <- stats.df$V1
+
+# Remove row names column
 stats.df <- stats.df[,2:length(stats.df[1,])]
+
+# Assign the correct column names
 colnames(stats.df) <- col_names
 
-fwrite(stats.df,file=paste(label,"_stats.csv",sep=""),sep=",")
+# Write the final stats out to a csv file
+fwrite(stats.df,file=paste(label,"_stats.csv",sep=""),sep=",",row.names = T)
 
-if (!(is.continuous)){
-  library(ggplot2)
+# Make some plots
+# Load ggplot for plots
+library(ggplot2)
+
+# Gather continuous covariates
+quant_covars <- covars[val.type == "continuous"]
+
+# Make sure we dont include the cohort column
+quant_covars = quant_covars[quant_covars != cohort_column]
+
+# Initialize the pdf
+pdf(paste(label,"_plots.pdf",sep=""),width=11)
+# if (length(quant_covars) > 1){
+#   layout(matrix(seq(1,6*(length(quant_covars)-1)),nrow=length(quant_covars)-1,ncol=6,byrow=T))
+# }else{
+#   layout(matrix(seq(1,6*(length(quant_covars))),nrow=length(quant_covars),ncol=6,byrow=T))
+# }
+
+# For each continuous covariate
+for (i in quant_covars){
+  print(i)
   
-  quant_covars <- covars[val.type == "continuous"]
-  quant_covars = quant_covars[quant_covars != cohort_column]
+  # Plot all samples
+  plot <- ggplot(ped.data, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
+  print(plot + geom_boxplot() + labs(title = "All samples",x="Cohort",y=i))
   
-  ped.data <- ped.data[!is.na(ped.data[,outcome]),]
-  case = ped.data[ped.data[,outcome] == 2 | ped.data[,outcome] == 1,]
-  control = ped.data[ped.data[,outcome] == 0,]
-  
-  male.val <- "M"
-  female.val <- "F"
-  
-  m = ped.data[ped.data$sex==male.val,]
-  f = ped.data[ped.data$sex==female.val,]
-  
-  if (NCOL(m)==0 && NCOL(f)==0){
-    male.val <- 1
-    female.val <- 2
+  if (!(is.continuous)){
+  # Plot samples devided by outcome    
+  plot <- ggplot(ped.data, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1))
+  print(plot + geom_boxplot(aes(fill=factor(get(outcome)))) + labs(title = "All samples by outcome",x="Cohort",y=i,fill=aes_string(outcome)))
   }
-  
-  m_case = case[case$sex==male.val,]
-  m_control = control[control$sex==male.val,]
-  f_case = case[case$sex==female.val,]
-  f_control = control[control$sex==female.val,]
-  
-  
-  
-  pdf(paste(label,"_plots.pdf",sep=""),width=11)
-  if (length(quant_covars) > 1){
-    layout(matrix(seq(1,6*(length(quant_covars)-1)),nrow=length(quant_covars)-1,ncol=6,byrow=T))
-  }else{
-    layout(matrix(seq(1,6*(length(quant_covars))),nrow=length(quant_covars),ncol=6,byrow=T))
-  }
-  for (i in quant_covars){
-    print(i)
-    # 
-    # plot <- ggplot(ped.data, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    # print(plot + geom_violin() + labs(title = "All samples"))
-    # 
-    # 
-    # plot <- ggplot(case, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    # print(plot + geom_violin() + labs(title = "Case samples"))
-    # 
-    # 
-    # plot <- ggplot(control, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    # print(plot + geom_violin() + labs(title = "Control samples"))
-    # 
-    # plot <- ggplot(ped.data, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    # print(plot + geom_violin(aes(fill=factor(get(outcome)))) + labs(title = "All samples"))
-    # 
-    # 
-    # plot <- ggplot(ped.data, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    # print(plot + geom_violin(aes(fill=factor(sex))) + labs(title = "All samples by sex"))
-    # 
-    # 
-    # plot <- ggplot(case, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    # print(plot + geom_violin(aes(fill=factor(sex))) + labs(title = "Case samples by sex"))
-    # 
-    # 
-    # plot <- ggplot(control, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    # print(plot + geom_violin(aes(fill=factor(sex))) + labs(title = "Control samples by sex"))
-    
-    
-    
-    plot <- ggplot(ped.data, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    print(plot + geom_boxplot() + labs(title = "All samples"))
-    
-    
-    plot <- ggplot(case, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    print(plot + geom_boxplot() + labs(title = "Case samples"))
-    
-    
-    plot <- ggplot(control, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    print(plot + geom_boxplot() + labs(title = "Control samples"))
-    
-    plot <- ggplot(ped.data, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    print(plot + geom_boxplot(aes(fill=factor(get(outcome)))) + labs(title = "All samples"))
-    
-    
-    plot <- ggplot(ped.data, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    print(plot + geom_boxplot(aes(fill=factor(sex))) + labs(title = "All samples by sex"))
-    
-    
-    plot <- ggplot(case, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    print(plot + geom_boxplot(aes(fill=factor(sex))) + labs(title = "Case samples by sex"))
-    
-    
-    plot <- ggplot(control, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
-    print(plot + geom_boxplot(aes(fill=factor(sex))) + labs(title = "Control samples by sex"))
-  }
-  
-  dev.off()
-} else {
-  pdf(paste(label,"_plots.pdf",sep=""),width=11)
-  fwrite(data.frame(na="NA"), file=paste(label,"_stats.csv",sep=""))
-  dev.off()
+  # Plot samples devided by sex
+  plot <- ggplot(ped.data, aes_string(cohort_column, i)) + theme(text = element_text(size=10), axis.text.x = element_text(angle=90, hjust=1)) 
+  print(plot + geom_boxplot(aes(fill=factor(sex))) + labs(title = "All samples by sex",x="Cohort",y=i,fill="sex"))
 }
+
+dev.off()
+
