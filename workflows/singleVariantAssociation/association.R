600eaf6ec63c434b0d9285899cb97f0597a1be3b
Timothy Majarian <tmajaria@wm059-b32.fios-router.home>
2018-01-15 10:08:43 -0500

    clean up of old code, removed unecessarry inputs

--- workflows/singleVariantAssociation/association.R
+++ workflows/singleVariantAssociation/association.R
@@ -1,182 +1,60 @@
+# association.R
 
-# GENESIS-test-v3.R
-
-# INPUT:
-# (1) a genotype (gds) file
-# (2) a phenotype (ped) file
-# (3) a GRM kinship.objects.RData file (from kinshipMatrix.R file)
-# (4) a commonIDs .RData file
-# (5) the name of the column containing the IDs in the ped file
-# (6) a label describing the workflow (for naming the assoc results),
-# (7) the outcome under study
-# (8) the outcome type (dichot/cont), 
-# (9) covariates to use, separated by "," and no space. Example: "age,sexTrait,BMI"   
-
-# OUTPUT:
-# An assoc.RData file
-
-# command line example
-# R --vanilla --args \
-#  GoT2D.chr22.biallelic.gds \
-#  GoT2D.phenotype.ped \
-#  GoT2D_T2D.kinship.objects.RData \ 
-#  GoT2D_T2D.commonIDs.RData \
-#  GoT2DsampleID \
-#  GoT2D_T2D \
-#  T2D \ 
-#  dichotomous \
-#  age sex BMI 
-
+# Load packages
 library(GENESIS)
 library(GWASTools)
 library(SeqArray)
 library(SeqVarTools)
 library(data.table)
 
+# Parse input arguments
 input_args <- commandArgs(trailingOnly=T)
+gds.file <- input_args[1] 
+null.file <- input_args[2]
+label <- input_args[3]
+test <- input_args[4]
+mac <- as.numeric(input_args[5])
 
-gds <- input_args[1] #"GoT2D.chr22.biallelic.gds"
-ped <- input_args[2] #"GoT2D.phenotype.ped"
-kinship_file <- input_args[3] # GoT2DSampleID
-commonID_file <- input_args[4]
-id.column.name <- input_args[5]
-label <- input_args[6]
-outcome <- input_args[7]
-outcomeType <- input_args[8]
-minMAC <- as.numeric(input_args[9]) # hard coded
-covariates <- NULL
+# Load nullfile
+load(null.file)
 
-print(paste("GDS file:",gds))
-print(paste("Ped file:",ped))
-print(paste("Kinship file:",kinship_file))
-print(paste("Common IDs file:",commonID_file))
-print(paste("ID column name:",id.column.name))
-print(paste("label:",label))
-print(paste("outcome:",outcome))
-print(paste("outcomeType:",outcomeType))
+# Open gds file
+gds.data <- seqOpen(gds.file)
 
+# Filter by desired MAC
+seqSetFilter(gds.data,sample.id=nullmod$scanID, action="intersect", verbose=TRUE)
+gds.freq <- seqAlleleFreq(gds.data, .progress=TRUE)
+gds.maf <- pmin(gds.freq, 1-gds.freq)
+gds.mac.filt <- 2 * gds.maf * (1-gds.maf) * length(nullmod$scanID) >= mac
 
-if(length(input_args)>9) {
+# If no snps remain, return empty
+if(sum(gds.mac.filt)==0) {
+	print("No SNPs pass MAC filter. Finished Association Step")
+	assoc <- NA
+
+# Else move on to association testing
+} else {
+
+	# Filter to snps with mac greater than threshold
+	seqSetFilter(gds.data, variant.sel=gds.mac.filt, action="intersect", verbose=TRUE)
+
+	# Organize data for output
+	pos <- seqGetData(gds.data,"position")
+	allele <- seqGetData(gds.data, "allele")
+	snps.pos <- cbind(pos,allele)
 	
-	covariates <- strsplit(input_args[10],split=",")[[1]]
-	print(paste("covariates:",paste(covariates,collapse=" ")))
+	# Print the number of snps were working with
+	print("Filtered SNPs")
+	print(dim(snps.pos))
 
-}
+	# Genotype data to the correct format
+	gds.geno.data <- SeqVarData(gds.data)
 
-
-
-# load ped_file and re-order columns
-# ped_file <- read.table(ped, header = TRUE, as.is = FALSE)
-ped_file <- fread(ped,sep="\t",header=T,stringsAsFactors=FALSE,showProgress=TRUE,data.table=FALSE)
-ped_file <- ped_file[!duplicated(ped_file[,id.column.name]),]
-rownames(ped_file) <- ped_file[,id.column.name]
-head(ped_file)
-
-# load common ID and kinship files
-load(commonID_file)
-
-library(gdsfmt)
-geno.pcrelate <- openfn.gds(kinship_file)
-# load(paste(kinship_file)) #ped_file.trimmed, PCRgrm, filteredSNPs, both
-  
-## trim phenotype file 
-ped_file.trimmed <- ped_file[ped_file[,id.column.name] %in% both, ]
-ped_file.trimmed <- ped_file.trimmed[both,]
-
-
-## load gds file using GWASTools/GENESIS
-#geno <- GdsGenotypeReader(filename=gds)
-#genoData <- GenotypeData(geno)
-
-# convert phenotype dataframe to ScanAnnotationDataFrame object
-if(!is.null(covariates)) {
-	df <- cbind(setNames(data.frame(both,ped_file.trimmed[,outcome]), c("scanID",outcome)), covs = ped_file.trimmed[,covariates])
-
-	# change covariates = [age, sex, BMI] to covariates = [covs.age, covs.sex, covs.BMI]
-	covariates <- paste("covs.", covariates, sep="")
-
-	# double check sex column;
-	if("covs.sex" %in% covariates) {
-		print("Sex is one of the covariates")
-		if(sum(c(1,2) %in% names(table(df[,"covs.sex"])))==2) {
-			df$sex <- ifelse(df[,"covs.sex"]==2,"F","M")
-			print("Converted sex from 1/2 to M/F")
-			print(table(sex=df$sex,covs.sex=df$covs.sex,useNA="always"))
-		} else {
-			df$sex <- df$covs.sex
-			print(table(sex=df$sex,covs.sex=df$covs.sex,useNA="always"))
-		}
-		covariates <- c(setdiff(covariates,"covs.sex"),"sex")
-		print(paste("covariates:",paste(covariates,collapse=" ")))	
-	}
-} else {
-	df <- setNames(data.frame(both,ped_file.trimmed[,outcome]), c("scanID",outcome))
-
-}
-
-scanAnnot <- ScanAnnotationDataFrame(df)
-str(scanAnnot)
-
-#### Null model - Future Versions of this script can be extended to input null models
-
-# get GRM in correct format
-PCRgrm <- pcrelateMakeGRM(geno.pcrelate, scan.include = both, scaleKin = 2)
-
-if(outcomeType=="dichotomous" & is.null(covariates)) {
-	        nullmod <- fitNullMM(scanData = scanAnnot, outcome = outcome, family = binomial, covMatList = PCRgrm)
-}
-if(outcomeType=="dichotomous" & !is.null(covariates)) {
-	        nullmod <- fitNullMM(scanData = scanAnnot, outcome = outcome, family = binomial, covMatList = PCRgrm, covars = covariates)
-}
-if(outcomeType=="continuous" & !is.null(covariates)) {
-	        nullmod <- fitNullMM(scanData = scanAnnot, outcome = outcome, family = gaussian, covMatList = PCRgrm, covars = covariates)
-}
-
-if(outcomeType=="continuous" & is.null(covariates)) {
-	        nullmod <- fitNullMM(scanData = scanAnnot, outcome = outcome, family = gaussian, covMatList = PCRgrm)
-}
-
-
-#### run association test
-geno <- seqOpen(gds)
-
-# filter by MAF
-seqSetFilter(geno,sample.id=nullmod$scanID, action="intersect", verbose=TRUE)
-
-ref.freq <- seqAlleleFreq(geno, .progress=TRUE)
-maf <- pmin(ref.freq, 1-ref.freq)
-maf.filt <- 2 * maf * (1-maf) * length(nullmod$scanID) >= minMAC
-print(table(maf.filt))
-
-if(sum(maf.filt)==0) {
-print("No SNPs pass MAC filter. Finished Association Step")
-assoc <- NA
-
-} else {
-seqSetFilter(geno, variant.sel=maf.filt, action="intersect", verbose=TRUE)
-
-## add position and rsID
-pos <- seqGetData(geno,"position")
-#chr.pos <- seqGetData(geno, "$chrom_pos") This line didn't seem to work after filtering geno
-allele <- seqGetData(geno, "allele")
-snps.pos <- cbind(pos,allele)
-print("Filtered SNPs")
-print(dim(snps.pos))
-
-genoData <- SeqVarData(geno)
-
-## dichotomous 
-if(outcomeType=="dichotomous" ) {
-	assoc <- assocTestMM(genoData = genoData, nullMMobj = nullmod, test = "Score")
-}
-
-## continuous 
-if(outcomeType=="continuous" ) {
-	assoc <- assocTestMM(genoData = genoData, nullMMobj = nullmod, test = "Wald")
-}
-print("Finished Association Step")
-print(dim(assoc))
-assoc <- cbind(snps.pos, assoc)
+	# Run association test
+	assoc <- assocTestMM(genoData = gds.geno.data, nullMMobj = nullmod, test = test)
+	print("Finished Association Step")
+	print(dim(assoc))
+	assoc <- cbind(snps.pos, assoc)
 
 }
 ## save assoc object
