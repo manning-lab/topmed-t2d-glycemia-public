b29e666eb384f746cd131212526d1f7f4d9397e9
Timothy Majarian <tmajaria@wm059-b32.fios-router.home>
2018-01-22 10:45:05 -0500

    major changes to aggregate association to match single var association

--- workflows/aggregateAssociation/aggregateAssociation.R
+++ workflows/aggregateAssociation/aggregateAssociation.R
@@ -1,42 +1,55 @@
+# aggAssociation.R
+
+# Load packages
 library(GENESIS)
 library(GWASTools)
 library(SeqArray)
 library(SeqVarTools)
 library(data.table)
 
+##### testing inputs
+# gds.file <- "/Users/tmajaria/Documents/projects/topmed/results/varshney/genomewide_v1/group_check/freeze4.chr21.pass.gtonly.minDP10.genotypes.gds"
+# null.file <- "/Users/tmajaria/Documents/projects/topmed/data/test_inputs/null_models/Model15_Sing_F4_sampAFEU_kinsGRM_ALL_cov.age.sex.study_pcNONE.Rda"
+# label <- "group_test"
+# test <- "SKAT"
+# pval <- "kuonen"
+# group.file <- "/Users/tmajaria/Documents/projects/topmed/results/varshney/genomewide_v2/groups/groups_16.RData"
+# weights <- as.numeric(unlist(strsplit("1,50",",")))
+#####
+
+# Parse input args
 input_args <- commandArgs(trailingOnly=T)
+gds.file <- input_args[1] 
+null.file <- input_args[2]
+group.file <- input_args[3]
+label <- input_args[4]
+test <- input_args[5]
+pval <- input_args[6]
+weights <- as.numeric(unlist(strsplit(input_args[7],",")))
 
-gds <- input_args[1] #"GoT2D.chr22.biallelic.gds"
-label <- input_args[2]
-test <- input_args[3]
-pval <- input_args[4]
-group.file <- input_args[5]
-null.file <- input_args[6]
 
-## load gds file using GWASTools/GENESIS
-geno <- seqOpen(gds)
-genoData <- SeqVarData(geno)
+# Load nullfile
+load(null.file)
+
+# Open gds file
+gds.data <- seqOpen(gds.file)
+
+# Filter to samples in null model
+seqSetFilter(gds.data,sample.id=nullmod$scanID, action="intersect", verbose=TRUE)
+
+# Genotype data to the correct format
+gds.geno.data <- SeqVarData(gds.data)
 
 ## load groups
 group_ext <- tail(unlist(strsplit(basename(group.file),'\\.')),n=1)
 if (group_ext == 'RData'){
+  # load if RData file
   load(group.file)  
-  
+
 } else if (group_ext == 'tsv') {
-  group.raw <- fread(group.file,sep="\t", data.table=F)
-  var.df <- data.frame(id = seqGetData(geno, "variant.id"), pos = seqGetData(geno, "position"), ref = refChar(geno), alt = altChar(geno))
-  var.df <- var.df[var.df$pos %in% group.raw$position,]
-  group.raw <- group.raw[group.raw$position %in% var.df$pos,]
-  group.var <- merge(group.raw, var.df, by.x=c('position','ref','alt'), by.y=c('pos','ref','alt'))
-  groups <- list()
-  
-  for (gid in unique(groups.var$group_id)){
-    groups[[gid]] <- groups.var[groups.var$group_id == gid,]
-  }
-  
-} else if (group_ext == 'csv') {
-  group.raw <- fread(group.file,sep=",", data.table=F)
-  var.df <- data.frame(id = seqGetData(geno, "variant.id"), pos = seqGetData(geno, "position"), ref = refChar(geno), alt = altChar(geno))
+  # load with data table
+  group.raw <- fread(group.file, data.table=F)
+  var.df <- data.frame(id = seqGetData(gds.data, "variant.id"), pos = seqGetData(gds.data, "position"), ref = refChar(gds.data), alt = altChar(gds.data))
   var.df <- var.df[var.df$pos %in% group.raw$position,]
   group.raw <- group.raw[group.raw$position %in% var.df$pos,]
   group.var <- merge(group.raw, var.df, by.x=c('position','ref','alt'), by.y=c('pos','ref','alt'))
@@ -50,18 +63,16 @@ if (group_ext == 'RData'){
   stop("Group file does not have the required extension")
 }
 
-groups <- groups[!duplicated(names(groups))]
-
-## load null model
-load(null.file)
+groups = groups[!duplicated(names(groups))]
 
 #### run association test
 if(test=="SKAT"){
-  assoc <- assocTestSeq(genoData, nullmod, groups, test=test, pval.method=pval, weight.beta = c(1,25))
+  assoc <- assocTestSeq(gds.geno.data, nullmod, groups, test=test, pval.method=pval, weight.beta = weights)
+  assoc$results = assoc$results[order(assoc$results$pval_0),]
   save(assoc, file=paste(label, ".assoc.RData", sep=""))
 }
 
-seqClose(geno)
+seqClose(gds.data)
 
 
 
