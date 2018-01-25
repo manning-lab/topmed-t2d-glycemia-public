cb535342a49b454c7d4432672f4c21bfc0a146bd
Timothy Majarian <tmajaria@broadinstitute.org>
2018-01-25 14:30:55 -0500

    commented out testing inputs...

--- workflows/singleVariantAssociation/preprocess_conditional.R
+++ workflows/singleVariantAssociation/preprocess_conditional.R
@@ -24,11 +24,11 @@ snps <- unlist(strsplit(input_args[5],","))
 label <- input_args[6]
 
 #### testing inputs
-gds.files <- c("/Users/tmajaria/Documents/projects/topmed/data/test_inputs/gds_files/chunk1.freeze4.chrALL.pass.gtonly.minDP10.genotypes.gds")
-phenotype.file <- "/Users/tmajaria/Documents/projects/topmed/data/test_inputs/phenotypes/Pooled_AFEU_WesselJ_25AUG2017_T2D.csv"
-id.col <- "topmedid"
-sample.file <- "/Users/tmajaria/Documents/projects/topmed/data/test_inputs/phenotypes/Pooled_AFEU_WesselJ_25AUG2017_T2D.txt"
-snps <- unlist(strsplit("1:10485,1:10488",","))
+# gds.files <- c("/Users/tmajaria/Documents/projects/topmed/data/test_inputs/gds_files/chunk1.freeze4.chrALL.pass.gtonly.minDP10.genotypes.gds")
+# phenotype.file <- "/Users/tmajaria/Documents/projects/topmed/data/test_inputs/phenotypes/Pooled_AFEU_WesselJ_25AUG2017_T2D.csv"
+# id.col <- "topmedid"
+# sample.file <- "/Users/tmajaria/Documents/projects/topmed/data/test_inputs/phenotypes/Pooled_AFEU_WesselJ_25AUG2017_T2D.txt"
+# snps <- unlist(strsplit("1:10485,1:10488",","))
 ####
 
 # get the snps into a dataframe
