494bb07c4c8b0d7111fea44b51319ecf8c48df5d
Timothy Majarian <tmajaria@broadinstitute.org>
2018-01-29 09:35:08 -0500

    fixed NA recognition in fitnull

--- workflows/fitNull/genesis_nullmodel.R
+++ workflows/fitNull/genesis_nullmodel.R
@@ -41,7 +41,7 @@ suppressMessages(library(GWASTools))
 covariates <- unlist(strsplit(covariate.string,","))
 
 # If this is conditional, combine with covariates
-if (!(conditional.string == "NA"))
+if (!(conditional.string == "NA")) {
   conditional = unlist(strsplit(conditional.string,","))
   
   # check that the conditional covars start with a letter, if not, add chr
