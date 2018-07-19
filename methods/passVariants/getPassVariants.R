a91fe07fa3235ed0f4c670a5807ed8c737a19301
Timothy Majarian <tmajaria@broadinstitute.org>
2018-07-19 15:09:37 -0400

    -

--- methods/passVariants/getPassVariants.R
+++ methods/passVariants/getPassVariants.R
@@ -32,6 +32,7 @@ chr <- paste0("chr",df$chromosome[1])
 df <- df[df$filter == "PASS",]
 df$MarkerName <- paste("chr", df$chromosome, "-", df$position, "-", df$ref, "-", df$alt, sep = "")
 df <- df[, "MarkerName"]
+df <- unique(df)
 
 fwrite(list(df), file = paste0("freeze.5b.passing.variants.", chr, ".minDP10.csv"), sep = "\t", col.names = F, quote = F)
 
