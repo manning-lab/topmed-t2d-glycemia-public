8d8ed831f7c951c2dc8deae11c041003fa35adb9
Timothy Majarian <tmajaria@broadinstitute.org>
2018-07-20 11:40:45 -0400

    fixed gsub

--- methods/passVariants/filterAssocPassingMetal.R
+++ methods/passVariants/filterAssocPassingMetal.R
@@ -3,6 +3,9 @@ args <- commandArgs(trailingOnly=T)
 res.file <- args[1]
 pass.file <- args[2]
 
+res.file <- "/Users/tmajaria/Documents/projects/public_workflows/fineMap/paintor/test_inputs/t2d_Model4_cov_age_sex_topmedproject_ALLT2D_selfreportAncestry.METAL.assoc.csv"
+pass.file <- "/Users/tmajaria/Documents/projects/topmed/code/topmed-t2d-glycemia-public/methods/passVariants/test_inputs/freeze.5b.passing.variants.all.minDP10.csv"
+
 out.file <- basename(res.file)
 
 library(data.table)
@@ -13,7 +16,7 @@ pass.data <- fread(pass.file, data.table = F, stringsAsFactors = F, header = F)$
 
 # fix markers
 # chr10:112998590:C:T
-pass.data <- gsub(":","-",pass.data)
+pass.data <- gsub("-",":",pass.data)
 
 # keep passing variants
 res.data <- res.data[res.data$MarkerName %in% pass.data,]
